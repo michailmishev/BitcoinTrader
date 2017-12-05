1fae29cf4952f77e2b2a12d10908e2d5eaeb3efc
Michail Mishev <m.mishev1@gmail.com>
2017-12-05 11:50:21 +0200

    install Cocopods

--- BitcoinTrader/ViewController.swift
+++ BitcoinTrader/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+
+platform :ios, '9.0'
+
+target 'BitcoinTrader' do
+
+  use_frameworks!
+
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+
+end
